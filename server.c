e7681c74bcadc88ae33a945fcc84391316815243
ktvexe <ktvexe@gmail.com>
2015-10-10 21:50:27 +0800

    socket server v1.0X

---
+++ server.c
@@ -0,0 +1,16 @@
+/*
+*socket server v1.0
+*
+*
+*
+*
+*@author LG Liu
+*@link
+*/
+
+ #include <stdio.h>
+ #include <stdlib.h>
+ #include <string.h>
+ #include <unistd.h>
+ #include <sys/socket.h>
+
