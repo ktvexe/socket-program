adf51eee525a66e18d8807d0e74dd320f2156992
ktvexe <ktvexe@gmail.com>
2015-10-22 01:38:46 +0800

    easy online edit by socket

--- client.c
+++ client.c
@@ -1,23 +1,38 @@
 /*
 *socket client v1.0
 *
-*
-*
+*practice socket client to do a online editer
+*user can operate creating,editing,removing,listing or downloading file
 *
 *@author LG Liu
 *@link https://github.com/ktvexe/socket-program
 */
+
 #include <sys/types.h>          
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
+/*create socket interface to be connected
+  sockfd : socket descriptor
+*/
 void create_socket(int* sockfd);
+/*initialize the parameter of socket
+  client_name : basic struct of internet address
+*/
 void setting_sockaddr_in(struct sockaddr_in* client_name);
+/*connect to other socket*/
 void connect_to_server(int sockfd, struct sockaddr_in);
+/*send message buffer to other socket
+  fd : socket descriptor
+  buf[] :char array of message
+  size : size of char array of message
+*/
 void send_message(int fd,char buf[],int size);
+/*receive message buffer from other socket*/
 void receive_message(int fd,char buf[],int size);
+/*do requirement*/
 void switch_function(char* buf,int fd);
 
 int main(int argc, char* const argv[])
@@ -34,7 +49,6 @@ int main(int argc, char* const argv[])
     /* initialize structure sockadd_in */
 	setting_sockaddr_in(&client_name);
 	
-	//client don't have to bind,because which port client use is not important
 	/* Connecting to server */
 	connect_to_server(sockfd, client_name);
 
@@ -42,12 +56,12 @@ int main(int argc, char* const argv[])
     bzero(buffer, 1024);
 	receive_message(sockfd,buffer,sizeof(buffer));
     printf("%s", buffer);
-    
+    /*key in use requirement*/
     scanf("%c",choose);
 	printf("Your choice: %c\n",*choose);
-
+	/*send use choice to server*/
 	send(sockfd,choose,sizeof(choose),0);
-	
+	/*do requirement*/
 	switch_function(choose,sockfd);
 	/* Close connection */
 	close(sockfd);
@@ -56,7 +70,12 @@ int main(int argc, char* const argv[])
 
 
 void create_socket(int* sockfd){
-    *sockfd = socket(PF_INET, SOCK_STREAM, 0);
+    /*create socket and error checking
+	  PF_INET : communication domain
+	  SOCK_STREAM : communication semantics
+	  0 : a particular protocol to be used with the socket
+	*/
+	*sockfd = socket(PF_INET, SOCK_STREAM, 0);
 	if( sockfd < 0){
          printf("Create Socket Failed!\n");
          exit(1);
@@ -65,7 +84,12 @@ void create_socket(int* sockfd){
 
 
 void setting_sockaddr_in(struct sockaddr_in* client_name){
-    bzero(client_name, sizeof(*client_name));
+    /*initialize structure of socket 
+	  sin_family : family of address
+	  sin_port : port to connect
+	  sin_addr : IP address
+	*/
+	bzero(client_name, sizeof(*client_name));
     client_name->sin_family = AF_INET;
     client_name->sin_port = htons(3000);
     client_name->sin_addr.s_addr = inet_addr("127.0.0.1");
@@ -73,13 +97,17 @@ void setting_sockaddr_in(struct sockaddr_in* client_name){
 }
 
 void connect_to_server(int sockfd, struct sockaddr_in client_name){
-    if(connect(sockfd, (struct sockaddr*)&client_name, sizeof(client_name)) < 0){
+    /*connect to server and error checking*/
+	if(connect(sockfd, (struct sockaddr*)&client_name, sizeof(client_name)) < 0){
 		printf("Connect failed!\n");
 		exit(1);
 	}
 }
 
 void send_message(int fd,char* buf,int size){
+	/*send message
+	  0 : flag
+	*/
 	send(fd,buf,size,0);
     bzero(buf ,size );
 
@@ -87,6 +115,9 @@ void send_message(int fd,char* buf,int size){
 
 
 void receive_message(int fd,char buf[],int size){
+	/*receive message
+	  0 : flag
+	*/
 	int res=recv(fd, buf, size, 0);
     if(res < 0){
 		printf("Recieve failed!\n");
@@ -97,17 +128,20 @@ void receive_message(int fd,char buf[],int size){
 
 
 void switch_function(char* choose,int sockfd){
+	/*create file*/
 	if(*choose =='c'||*choose=='C'){
         char create[30] ;
 		bzero(create,30);
 		receive_message(sockfd,create,sizeof(create));
     	printf("%s\n", create);
         bzero( create,sizeof(create) );
+		/*key in file name and send to server*/
 		scanf("%s",create);
 		printf("Create file name : %s\n",create);
 		send_message(sockfd,create,sizeof(create));
     }
 
+    /*edit file*/
 	else if(*choose =='e'||*choose=='E'){
         char edit[128] ;
 		FILE* fp;
@@ -115,6 +149,7 @@ void switch_function(char* choose,int sockfd){
 		receive_message(sockfd,edit,sizeof(edit));
     	printf("%s", edit);
         bzero( edit,sizeof(edit) );
+		/*key in file name and send to server*/
 		scanf("%s",edit);
 		printf("Editing file : %s\n",edit);
 		send_message(sockfd,edit,sizeof(edit));
@@ -127,10 +162,8 @@ void switch_function(char* choose,int sockfd){
     	printf("%s\n", edit);
 		bzero(edit,sizeof(edit));
 		fgetc(stdin);
+		/*send edit content to server*/
 		while(1){
-		//	scanf("%s",edit);
-		//	scanf("%[^\n]%*c",edit);
-		//	scanf("%[^\n]",edit);
         	fgets(edit,sizeof(edit),stdin);
 			send(sockfd,edit,sizeof(edit),0);
 			int cmp =strcmp(edit,":end\n");
@@ -142,21 +175,24 @@ void switch_function(char* choose,int sockfd){
     	printf("Edit done\n");
     }
 	
+	/*delete file*/
 	else if(*choose =='r'||*choose=='R'){
         char remove_buffer[50] ;
 		bzero(remove_buffer,50);
 		receive_message(sockfd,remove_buffer,sizeof(remove_buffer));
     	printf("%s", remove_buffer);
         bzero( remove_buffer,sizeof(remove_buffer) );
+		/*key in file name and send to server*/
 		scanf("%s",remove_buffer);
 		printf("Remove file : %s\n",remove_buffer);
 		send_message(sockfd,remove_buffer,sizeof(remove_buffer));
     }
 
-
+	/*list file*/
 	else if(*choose =='l'||*choose =='L'){
 		char list[30];
 		bzero(list,30);
+		/*show the buffer from server*/
 		while(1){
 			receive_message(sockfd,list,sizeof(list));
 			int cmp =strcmp(list,":end");
@@ -169,7 +205,7 @@ void switch_function(char* choose,int sockfd){
 	}
 	
 
-
+	/*download file*/
 	else if(*choose =='d'||*choose =='D'){
 		char down[60];
 		FILE* fp;
@@ -177,8 +213,10 @@ void switch_function(char* choose,int sockfd){
 		receive_message(sockfd,down,sizeof(down));
     	printf("%s\n", down);
         bzero( down,sizeof(down) );
+		/*key in file name and send to server*/
 		scanf("%s",down);
         send(sockfd,down,sizeof(down),0);
+		/*open file and error checking*/
 		fp = fopen(down,"wb");
 		if(fp == NULL ){
 			printf("Can not open the file\n");
@@ -186,6 +224,7 @@ void switch_function(char* choose,int sockfd){
 		}
         bzero( down,sizeof(down) );
 		int down_len =0;
+		/*receive file content from server and write to client*/
     	while(down_len =recv(sockfd, down , 60, 0)){
 			if(down_len < 0){
 				printf("Recieve file failed!\n");
@@ -199,9 +238,11 @@ void switch_function(char* choose,int sockfd){
         	bzero(down,sizeof(down)); 	
 		}
 		printf("Receive file\n");
+		/*close file*/
 		fclose(fp);
 	}
 
+	/*quit socket*/
 	else if(*choose =='q'||*choose =='Q'){
 		char quit[30];
 		bzero(quit,30);
--- server.c
+++ server.c
@@ -1,8 +1,8 @@
 /*
 *socket server v1.0
 *
-*
-*
+*practice socket server to do a online editer
+*user can operate creating,editing,removing,listing or downloading file
 *
 *@author LG Liu
 *@link https://github.com/ktvexe/socket-program
@@ -15,19 +15,50 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <dirent.h>
-
-void create_socket(int* );
-void setting_sockaddr_in(struct sockaddr_in* );
-void assign_port(int* ,struct sockaddr_in* );
-void listen_to_client(int* );
-void accept_connect(int* ,int* ,struct sockaddr_in*,int*);
-int handle_receive_choice(int* ,char* );	
-void switch_function(int ,int ,int* );
+/*create socket interface to be connected
+  sockfd : socket descriptor
+*/
+void create_socket(int* sockfd);
+/*initialize the parameter of socket
+  server_name : basic struct of internet address
+*/
+void setting_sockaddr_in(struct sockaddr_in* server_name);
+/*selete port to be used
+  parameter : sockfd , server_name  
+*/
+void assign_port(int* sockfd,struct sockaddr_in* server_name);
+/*wait and allow client to connect
+  parameter : sockfd
+*/
+void listen_to_client(int* sockfd);
+/*accept client to do sth
+  clientfd :a new socket descriptor to connect client
+  client_addr :basic struct of sock
+  addrlen :the size of the struct pointed to by addr
+*/
+void accept_connect(int* clientfd,int* sockfd,struct sockaddr_in* client_addr,int* addrlen);
+/*receive buffer from client
+  buffer1 : store choice from client
+  return :the case of requirement
+*/
+int handle_receive_choice(int* clientfd,char* buffer1);	
+/*do requirement of client
+  choice_case : the case of requirement
+  end : a switch of quit
+*/
+void switch_function(int choice_case,int clientfd,int* end);
+/*send message buffer to other socket
+  fd : socket descriptor
+  buf[] :char array of message
+  size : size of char array of message
+*/
 void send_message(int fd,char buf[],int size);
+/*receive message buffer from other socket*/
 void receive_message(int fd,char buf[],int size);
 
 int main()
 {
+	/*basic variable*/
     int sockfd;
 	int end = 0;
     struct sockaddr_in server_name;
@@ -43,7 +74,7 @@ int main()
     /* Assign a port number to socket */
 	assign_port(&sockfd ,&server_name );
 
-    /* make it listen to socket with max 20 connections */
+    /* make it listen to socket */
 	listen_to_client(&sockfd );
 
 	printf("**********SOCKET SERVER**********\n");	
@@ -79,8 +110,12 @@ int main()
 
 
 void create_socket(int* sockfd){
-
-    if((*sockfd = socket(PF_INET, SOCK_STREAM, 0) ) <0 ){
+	/*create socket and error checking
+      PF_INET : communication domain
+	  SOCK_STREAM : communication semantics
+	  0 : a particular protocol to be used with the socket
+	*/
+	if((*sockfd = socket(PF_INET, SOCK_STREAM, 0) ) <0 ){
 		printf("Create socket failed!\n");
 		exit(1);
 	}
@@ -88,17 +123,20 @@ void create_socket(int* sockfd){
 
 
 void setting_sockaddr_in(struct sockaddr_in* server_name){
-
+	/*initialize structure of socket 
+	  sin_family : family of address
+	  sin_port : port to connect
+	  sin_addr : IP address
+	*/
 	bzero(server_name, sizeof(*server_name));
     server_name->sin_family = AF_INET;
     server_name->sin_port = htons(3000);
-    /* this line is different from client */
     server_name->sin_addr.s_addr = htons(INADDR_ANY);
 }
 
 
 void assign_port(int* sockfd,struct sockaddr_in* server_name){
-
+    /* Assign a port number to socket and error checking*/
     if(bind(*sockfd, (struct sockaddr*)server_name, sizeof(*server_name)) ){
 		printf("Server Bind Port failed!\n");
 		exit(1);
@@ -107,7 +145,7 @@ void assign_port(int* sockfd,struct sockaddr_in* server_name){
 
 
 void listen_to_client(int* sockfd){
-
+    /* listen to socket with max 20 connections and error checking*/
     if( listen(*sockfd, 20) ){
 		printf("Server Listen failed!\n");
 		exit(1);
@@ -116,6 +154,7 @@ void listen_to_client(int* sockfd){
 
 
 void accept_connect(int* clientfd,int* sockfd,struct sockaddr_in* client_addr,int* addrlen){
+        /* Wait and Accept connection and error checking*/
 	 *clientfd = accept(*sockfd, (struct sockaddr*)client_addr, addrlen);
 		if(*clientfd < 0){
 			printf("Server Accept failed!\n");
@@ -126,6 +165,7 @@ void accept_connect(int* clientfd,int* sockfd,struct sockaddr_in* client_addr,in
 
 int handle_receive_choice(int* clientfd,char* buffer1){
 	receive_message(*clientfd,buffer1,sizeof(buffer1));
+	/*classify requirement*/ 
 	if(*buffer1 =='c'||*buffer1=='C'){
 		return 1;
 	}
@@ -151,26 +191,31 @@ int handle_receive_choice(int* clientfd,char* buffer1){
 
 void switch_function(int choice_case ,int clientfd,int* end){
 	switch (choice_case){
+		/*create file*/
 		case 1:{
 			FILE *fp;
 			char create[30] = "What is the name of the file ?";
 			send_message(clientfd,create,sizeof(create));
 			receive_message(clientfd,create,sizeof(create));
+			/*open file and error checking*/
 			fp = fopen(create,"w");
 			if (fp ==NULL){
 				printf("Open File failed!\n");
 				exit(1);
 			}
+			/*close file*/
 			fclose(fp);
 			break;
 		}
-
+		
+		/*edit file*/
 		case 2:{
 			char edit[128] = "please key in the file name which you want to edit:\n";
 			char file_name[64];
 			FILE* fpr,*fpa;
 			send_message(clientfd,edit,sizeof(edit));
 			receive_message(clientfd,edit,sizeof(edit));
+			/*open file and error checking*/
 			fpr = fopen(edit,"r+");
 			if(fpr ==NULL){
         		bzero(edit ,sizeof(edit) );
@@ -186,6 +231,7 @@ void switch_function(int choice_case ,int clientfd,int* end){
 			strcpy(edit,"Please key in what you want to append to the file\nIf you work done,please key in :end to finish it");
 			send_message(clientfd,edit,sizeof(edit));
 			int edit_len =0;
+			/*write content of buffer from client to file*/
 			while(edit_len = recv(clientfd ,edit,sizeof(edit),0)){
 				if (edit_len < 0){
         			bzero(edit ,sizeof(edit) );
@@ -194,21 +240,23 @@ void switch_function(int choice_case ,int clientfd,int* end){
 					send(clientfd,edit,sizeof(edit),0);
 					exit(1);
 				}
+				/*check finish signal*/
 				int cmp = strcmp(edit,":end\n");
 				if(cmp ==0){
 					break;
 				}
-				//fprintf(fpa,"%s\n",edit);
 				fputs(edit,fpa);
 				bzero(edit,sizeof(edit));
 			
 			}
+			/*close file*/
 			fclose(fpa);
 			printf("edit done\n");
 
 			break;
 		}
 
+		/*delete file*/
 		case 3:{
 			char remove_buffer[50] = "which file do you want to delete ?";
 			send_message(clientfd,remove_buffer,sizeof(remove_buffer));
@@ -218,15 +266,19 @@ void switch_function(int choice_case ,int clientfd,int* end){
 			break;
 		}
 
+		/*list document of directory*/
 		case 4:{
 			char list[30] = "These are document of server:";
 			DIR* dir;
 			struct dirent* ptr;
+			/*current directory*/
 			dir = opendir(".");
 			send_message(clientfd,list,sizeof(list));
+			/*shift dir pointer to all document of directory*/
 			while((ptr =readdir(dir)) != NULL){
         		bzero(list ,sizeof(list) );
 				strcpy(list,ptr->d_name);
+				/*check current or last dir*/
 				if(strcmp(list,".") && strcmp(list,"..")){
 					send_message(clientfd,list,sizeof(list));
 				}
@@ -237,17 +289,20 @@ void switch_function(int choice_case ,int clientfd,int* end){
 
 			break;
 		}
-
+		
+		/*download file*/
 		case 5:{
 			char down[60] = " Please key in the file name which you want to download";
 			FILE *fp;	
 			send_message(clientfd,down,sizeof(down));
 			receive_message(clientfd,down,sizeof(down));
+			/*open file and error checking*/
 			fp =fopen(down,"rb");
 			if (fp == NULL){
 				printf("Can not find the file:%s\n",down);
 			}
 			else{
+				/*read the content of file and send to client*/
 				bzero(down,sizeof(down));
 				int file_len =0;
 				while((file_len = fread(down,sizeof(char),60,fp))>0){
@@ -258,11 +313,12 @@ void switch_function(int choice_case ,int clientfd,int* end){
 					bzero(down,sizeof(down));
 				}
 			}
+			/*close file*/
 			fclose(fp);
 
 			break;
 		}
-
+		/*quit socket*/
 		case 6:{
 			char quit[30] = "Are you sure to quit ? (Y/N)";
 			send_message(clientfd,quit,sizeof(quit));
@@ -282,6 +338,9 @@ void switch_function(int choice_case ,int clientfd,int* end){
 }
 
 void send_message(int fd,char* buf,int size){
+	/*send message
+	  0 : flag
+	*/
 	send(fd,buf,size,0);
     bzero(buf ,size );
 
@@ -289,6 +348,9 @@ void send_message(int fd,char* buf,int size){
 
 
 void receive_message(int fd,char buf[],int size){
+	/*receive message
+	  0 : flag
+	*/
 	int res=recv(fd, buf, size, 0);
     if(res < 0){
 		printf("Recieve failed!\n");
